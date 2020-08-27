4aa09612db935c3e86b7ee2ff437b991a374c55c
Rayhan0Islam0Shagor <67514865+Rayhan0Islam0Shagor@users.noreply.github.com>
2020-08-27 18:43:06 +0600

    logo resize

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand font-weight-bold" href="/home"><img src={logo} className="rounded-pill" height="60px" alt="" /></a>
+                <a className="navbar-brand font-weight-bold" href="/home"><img src={logo} className="rounded-pill" height="50px" alt="" /></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
